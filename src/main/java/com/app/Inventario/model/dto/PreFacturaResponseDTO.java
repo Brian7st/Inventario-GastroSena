08d576490e60a22c72c86a290073888d7e3aaf92
johan <johita12344779@gmail.com>
2025-12-11 11:24:19 -0500

    se agrego los mapper de prefactura toResponseDtoPreFactura, toEntity,updateEntityFromDTO

--- src/main/java/com/app/Inventario/model/dto/PreFacturaResponseDTO.java
+++ src/main/java/com/app/Inventario/model/dto/PreFacturaResponseDTO.java
@@ -1,6 +1,7 @@
 package com.app.Inventario.model.dto;
 
 
+import com.app.Inventario.model.entity.ProgramaFormacion;
 import com.app.Inventario.model.enums.EstadoPreFactura;
 import lombok.Data;
 
@@ -21,5 +22,5 @@ public class PreFacturaResponseDTO {
     private BigDecimal totalGeneral;
 
 
-    private ProgramaResponseDTO programa;
+    private ProgramaFormacion programa;
 }
